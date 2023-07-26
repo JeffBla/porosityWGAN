018d589885e4f5707d8a5e9652dff2e4181e381d
JeffBla <jeff920305@gmail.com>
2023-07-26 20:32:42 +0800

    add calPorosityFlow1

--- wgan_gp_rockLoad_percent.py
+++ wgan_gp_rockLoad_percent.py
@@ -46,7 +46,7 @@ parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first 
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
-parser.add_argument("--img_size", type=int, default=64, help="size of each image dimension")
+parser.add_argument("--img_size", type=int, default=128, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=1, help="number of image channels")
 parser.add_argument("--n_critic", type=int, default=5, help="number of training steps for discriminator per iter")
 parser.add_argument("--clip_value", type=float, default=0.01, help="lower and upper clip value for disc. weights")
@@ -79,22 +79,25 @@ class Generator(nn.Module):
 
         self.model = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d(opt.latent_dim, ngf * 8, 4, 1, 0, bias=False),
+            nn.ConvTranspose2d(opt.latent_dim, ngf * 16, 4, 1, 0, bias=False),
+            # nn.BatchNorm2d(ngf * 16),
+            nn.ReLU(True),
+            # state size. ``(ngf*16) x 4 x 4``
+            nn.ConvTranspose2d(ngf * 16, ngf * 8, 4, 2, 1, bias=False),
             # nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
-            # state size. ``(ngf*8) x 4 x 4``
+            # state size. ``(ngf*8) x 8 x 8``
             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
             # nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
-            # state size. ``(ngf*4) x 8 x 8``
+            # state size. ``(ngf*4) x 16 x 16``
             nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             # nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
-            # state size. ``(ngf*2) x 16 x 16``
+            # state size. ``(ngf*2) x 32 x 32``
             nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
-            # nn.BatchNorm2d(ngf),
+            # nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
-            # state size. ``(ngf) x 32 x 32``
             nn.ConvTranspose2d(ngf, 3, 4, 2, 1, bias=False), # 3 for percent layer
             # nn.Tanh(),
             PercentLayer_dcgan()
@@ -112,23 +115,27 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
 
         self.model = nn.Sequential(
-            # input is ``(nc) x 64 x 64``
+            # input is ``(nc) x 128 x 128``
             nn.Conv2d(opt.channels, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. ``(ndf) x 32 x 32``
+            # state size. ``(ndf) x 64 x 64``
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
             # nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. ``(ndf*2) x 16 x 16``
+            # state size. ``(ndf*2) x 32 x 32``
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
             # nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. ``(ndf*4) x 8 x 8``
+            # state size. ``(ndf*4) x 16 x 16``
             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
             # nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. ``(ndf*8) x 4 x 4``
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            # state size. ``(ndf*8) x 8 x 8``
+            nn.Conv2d(ndf * 8, ndf * 16, 4, 2, 1, bias=False),
+            # nn.BatchNorm2d(ndf * 16),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. ``(ndf*16) x 4 x 4``
+            nn.Conv2d(ndf * 16, 1, 4, 1, 0, bias=False),
             )
 
     def forward(self, img):
@@ -142,10 +149,11 @@ lambda_gp = 10
 
 # Initialize generator and discriminator
 generator = Generator()
-generator.load_state_dict(torch.load('./model/rock/generator_percent_-1_1_allNoNorm.pt'))
+generator.load_state_dict(torch.load('./model/rock/generator_percent.pt'))
 discriminator = Discriminator()
-discriminator.load_state_dict(torch.load('./model/rock/discriminator_percent_-1_1_allNoNorm.pt'))
+discriminator.load_state_dict(torch.load('./model/rock/discriminator_percent.pt'))
 
+# extract percent
 features = []
 def hookForPercentLayerInput(module, input, output):
     features.append(input[0].clone().detach())
