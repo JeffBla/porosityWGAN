09d0763485d2f30735932ffc25f47b7a5b8ac361
JeffBla <jeff920305@gmail.com>
2023-08-31 16:07:56 +0800

    use the clip to train wgan
    also fix some bugs

--- wgan_gp_rockTrain_lightning_dcgan_383clip.py
+++ wgan_gp_rockTrain_lightning_dcgan_383clip.py
@@ -1,25 +1,13 @@
 # noinspection PyUnresolvedReferences
 from typing import Any, Optional
 from lightning.pytorch.utilities.types import STEP_OUTPUT, TRAIN_DATALOADERS
-import vtkmodules.vtkInteractionStyle
-# noinspection PyUnresolvedReferences
-import vtkmodules.vtkRenderingOpenGL2
-from vtkmodules.vtkIOImage import vtkDICOMImageReader
 
 import argparse
 import os
 import numpy as np
-import math
-import sys
-import matplotlib.pyplot as plt
-from pathlib import Path
 
-import torchvision.transforms as transforms
-from torchvision.utils import save_image
 import torchvision.utils as vutils
 
-from torch.utils.data import DataLoader, Dataset, random_split
-from torchvision import datasets
 from torch.autograd import Variable
 
 import torch.nn as nn
@@ -27,7 +15,6 @@ import torch.nn.functional as F
 import torch.autograd as autograd
 import torch
 
-import pytorch_lightning as pl
 import lightning as L
 
 from percentlayer import PercentLayer_dcgan
@@ -48,7 +35,7 @@ os.makedirs("images", exist_ok=True)
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataroot",
                     type=str,
-                    default='data/rockXCT',
+                    default='data/rockXCT_clip',
                     help="the target of data folder")
 parser.add_argument("--n_epochs",
                     type=int,
@@ -81,7 +68,7 @@ parser.add_argument("--latent_dim",
                     help="dimensionality of the latent space")
 parser.add_argument("--img_size",
                     type=int,
-                    default=128,
+                    default=382,
                     help="size of each image dimension")
 parser.add_argument("--channels",
                     type=int,
@@ -120,25 +107,28 @@ class Generator(nn.Module):
 
         self.model = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d(latent_dim, ngf * 16, 4, 1, 0, bias=False),
+            nn.ConvTranspose2d(latent_dim, ngf * 64, 4, 1, 0, bias=False),
             nn.ReLU(True),
             # state size. ``(ngf*16) x 4 x 4``
+            nn.ConvTranspose2d(ngf * 64, ngf * 32, 4, 2, 1, bias=False),
+            nn.ReLU(True),
+            # state size. ``(ngf*16) x 8 x 8``
+            nn.ConvTranspose2d(ngf * 32, ngf * 16, 4, 2, 1, bias=False),
+            nn.ReLU(True),
+            # state size. ``(ngf*16) x 16 x 16``
             nn.ConvTranspose2d(ngf * 16, ngf * 8, 4, 2, 1, bias=False),
             nn.ReLU(True),
-            # state size. ``(ngf*8) x 8 x 8``
+            # state size. ``(ngf*8) x 32 x 32``
             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
             nn.ReLU(True),
-            # state size. ``(ngf*4) x 16 x 16``
+            # state size. ``(ngf*4) x 64 x 64``
             nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.ReLU(True),
-            # state size. ``(ngf*2) x 32 x 32``
-            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
-            nn.ReLU(True),
-            # state size. ``(nc) x 64 x 64``
-            nn.ConvTranspose2d(ngf, 3, 4, 2, 1,
+            # state size. ``(ngf*2) x 128 x 128``
+            nn.ConvTranspose2d(ngf * 2, 3, 3, 3, 1,
                                bias=False),  # 3 for percent layer
             PercentLayer_dcgan()
-            # state size. ``(nc) x 128 x 128``
+            # state size. ``(nc) x 382 x 382``
         )
 
     def forward(self, z):
@@ -152,27 +142,26 @@ class Discriminator(nn.Module):
         super().__init__()
 
         self.model = nn.Sequential(
-            # input is ``(nc) x 128 x 128``
-            nn.Conv2d(img_shape[0], ndf, 4, 2, 1, bias=False),
+            # input is ``(nc) x 382 x 382``
+            nn.Conv2d(img_shape[0], ndf, 3, 3, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. ``(ndf) x 64 x 64``
+            # state size. ``(ndf*2) x 128 x 128``
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            # nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. ``(ndf*2) x 32 x 32``
+            # state size. ``(ndf*4) x 64 x 64``
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            # nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. ``(ndf*4) x 16 x 16``
+            # state size. ``(ndf*8) x 32 x 32``
             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            # nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. ``(ndf*8) x 8 x 8``
+            # state size. ``(ndf*16) x 16 x 16``
             nn.Conv2d(ndf * 8, ndf * 16, 4, 2, 1, bias=False),
-            # nn.BatchNorm2d(ndf * 16),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. ``(ndf*16) x 4 x 4``
-            nn.Conv2d(ndf * 16, 1, 4, 1, 0, bias=False),
+            # state size. ``(ndf*32) x 8 x 8``
+            nn.Conv2d(ndf * 16, ndf * 32, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. ``(ndf*64) x 4 x 4``
+            nn.Conv2d(ndf * 32, 1, 4, 1, 0, bias=False),
         )
 
     def forward(self, img):
